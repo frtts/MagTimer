0972a05062f28b4f0b99c5c8bcb3fa359d885702
furutatsu <nyannyanboo@gmail.com>
2012-06-11 23:51:15 +0900

    add project

---
+++ MagTimer/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MagTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.st_stp_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(138, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // st_stp_btn
+            // 
+            this.st_stp_btn.Location = new System.Drawing.Point(149, 11);
+            this.st_stp_btn.Name = "st_stp_btn";
+            this.st_stp_btn.Size = new System.Drawing.Size(75, 23);
+            this.st_stp_btn.TabIndex = 1;
+            this.st_stp_btn.Text = "Start";
+            this.st_stp_btn.UseVisualStyleBackColor = true;
+            this.st_stp_btn.Click += new System.EventHandler(this.st_stp_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0% まであと";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 19);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "%";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 93);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(158, 10);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(208, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "%";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.textBox2.Location = new System.Drawing.Point(180, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 16);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 125);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.st_stp_btn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "マグタイマー PSO2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button st_stp_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MagTimer/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MagTimer
+{
+    public partial class Form1 : Form
+    {
+        DateTime from;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 500;
+            timer1.Enabled = false;
+            st_stp_btn.Enabled = false;
+            from = new DateTime(1,1,1);
+            label1.Text = string.Format("{0:D2}:{1:D2}", from.Minute, from.Second);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.MinValue < from)
+            {
+                from = from.AddMilliseconds(-timer1.Interval);
+                double minsec = from.Minute + from.Second / 60.0;
+                label1.Text = string.Format("{0:D2}:{1:D2}", from.Minute, from.Second);
+                double current = minsec / 0.4;
+                progressBar1.Value = (int)current;
+                textBox2.Text = current.ToString("###.#");
+            }
+            else
+            {
+                timer1.Enabled = false;
+                st_stp_btn.Text = "Start";
+                st_stp_btn.Enabled = false;
+            }
+        }
+
+        private void st_stp_btn_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                // run -> stop
+                timer1.Enabled = false;
+                st_stp_btn.Text = "Start";
+            }
+            else
+            {
+                // stop -> run
+                timer1.Enabled = true;
+                st_stp_btn.Text = "Stop";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // only number, but character(like aaa or 123e) to negative number -1
+            int per;
+            if (!int.TryParse(textBox1.Text, out per)) per = -1;
+
+            // %(0 ~ 100) -> time (~ 40:00)
+            if (per >= 0 && per <= 100)
+            {
+                progressBar1.Value = per;
+                textBox2.Text = per.ToString("###.#"); // XXX
+                double m = 0.4 * per; // 100(%):40(min) = 1:0.4 => 0.4(min/%)
+                from = new DateTime(1, 1, 1, 0, (int)m, (int)((m - (int)m) * 60), 0);
+                label1.Text = string.Format("{0:D2}:{1:D2}", from.Minute, from.Second);
+                st_stp_btn.Enabled = true;
+            }
+        }
+    }
+}
